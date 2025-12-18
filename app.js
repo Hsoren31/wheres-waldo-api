b8dfd735abe028695ec33b5d8d4d92d496eaf475
Hsoren31 <sorensenhaley@gmail.com>
2025-12-18 15:02:43 -0700

    FEAT: build express server and routes
    Built two routes: one to get the stage and the other to check if a users
    guess matches the character location. There is a validation that the
    guess will run through to check the data coming in is a name and an
    object with number values.

---
+++ app.js
@@ -0,0 +1,69 @@
+import _ from "lodash";
+import { prisma } from "./lib/prisma.js";
+import express from "express";
+import { body, check, validationResult, matchedData } from "express-validator";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const validateGuess = [
+  body("character").trim().isAlpha(),
+  check("guess.x").isNumeric(),
+  check("guess.y").isNumeric(),
+];
+
+app.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const stage = await prisma.stage.findFirst({
+    where: {
+      id,
+    },
+    select: {
+      title: true,
+      image: true,
+      leaderBoard: true,
+    },
+  });
+  if (!stage) {
+    return res.status(404).send("Not Found");
+  }
+  res.json(stage);
+});
+
+app.post("/:stageId", [
+  validateGuess,
+  async (req, res) => {
+    const { stageId } = req.params;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send("Wrong Data. Try Again");
+    }
+    const { character, guess } = matchedData(req);
+    const { location } = await prisma.character.findFirst({
+      where: {
+        stageId,
+        name: character,
+      },
+      select: {
+        location: true,
+      },
+    });
+    if (_.isEqual(guess, location)) {
+      return res.json({ result: true });
+    }
+    res.json({ result: false });
+  },
+]);
+
+app.get("/{*splat}", (req, res) => {
+  res.status(404);
+});
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`My express app`);
+});
