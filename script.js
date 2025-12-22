cc23fcc07e47fd0c11bf0bf925b8fb5f1752a8c7
Hsoren31 <sorensenhaley@gmail.com>
2025-12-22 14:18:47 -0700

    FEAT: add prisma connection string

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-import { prisma } from "./lib/prisma.js";
+import "dotenv/config";
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   await prisma.stage.create({
