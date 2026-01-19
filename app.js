cdf99f94c6b419a9887ec929f6e5a80c046f4edd
Hsoren31 <sorensenhaley@gmail.com>
2026-01-19 16:11:31 -0700

    FEAT: add controller for starting/playing game
    The game functionality now operates on the backend rather than frontend.
    Giving more control and keeping characters locations more secret.

--- app.js
+++ app.js
@@ -1,63 +1,22 @@
 import _ from "lodash";
 import "dotenv/config.js";
-import { prisma } from "./lib/prisma.js";
 import express from "express";
 import session from "express-session";
-import { body, check, validationResult, matchedData } from "express-validator";
+import { startGame, validateGuess } from "./controllers/stageController.js";
 
 const app = express();
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const validateGuess = [
-  body("character").trim().isAlpha(),
-  check("guess.x").isNumeric(),
-  check("guess.y").isNumeric(),
-];
-
-app.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const stage = await prisma.stage.findFirst({
-    where: {
-      id,
-    },
-    select: {
-      title: true,
-      image: true,
-      leaderBoard: true,
-    },
-  });
-  if (!stage) {
-    return res.status(404).send("Not Found");
-  }
-  res.json(stage);
-});
-
-app.post("/:stageId", [
-  validateGuess,
-  async (req, res) => {
-    const { stageId } = req.params;
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).send("Wrong Data. Try Again");
-    }
-    const { character, guess } = matchedData(req);
-    const { location } = await prisma.character.findFirst({
-      where: {
-        stageId,
-        name: character,
-      },
-      select: {
-        location: true,
-      },
-    });
-    if (_.isEqual(guess, location)) {
-      return res.json({ result: true });
-    }
-    res.json({ result: false });
-  },
-]);
+app.get("/:stageId", startGame);
+app.post("/:stageId", validateGuess);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
---
+++ controllers/stageController.js
@@ -0,0 +1,66 @@
+import { prisma } from "../lib/prisma.js";
+
+async function startGame(req, res) {
+  try {
+    const { stageId } = req.params;
+    const stage = await prisma.stage.findFirst({
+      where: { id: stageId },
+      include: {
+        image: true,
+        characters: {
+          include: {
+            location: false,
+          },
+        },
+      },
+    });
+    stage.characters = stage.characters.map((character) => {
+      return { ...character, found: false };
+    });
+    stage.gameId = crypto.randomUUID();
+    req.session.game = stage;
+    res.send(stage);
+  } catch (err) {
+    res.status(404).send("Stage not found.");
+  }
+}
+
+async function validateGuess(req, res) {
+  try {
+    const stageId = req.session.game.id;
+    const { characterName, locationGuess } = req.body;
+    const { location } = await prisma.character.findFirst({
+      where: {
+        stageId,
+        name: characterName,
+      },
+      select: {
+        location: true,
+      },
+    });
+    if (
+      locationGuess.x >= location.x - 50 &&
+      locationGuess.x <= location.x + 50
+    ) {
+      if (
+        locationGuess.y >= location.y - 50 &&
+        locationGuess.y <= location.y + 50
+      ) {
+        req.session.game.characters = req.session.game.characters.map(
+          (character) => {
+            if (character.name === characterName) {
+              return { ...character, found: true };
+            }
+            return character;
+          }
+        );
+        return res.send("You Found character");
+      }
+    }
+    res.send("Try again.");
+  } catch (err) {
+    res.status(404).send("Could not find character. Try again.");
+  }
+}
+
+export { startGame, validateGuess };
