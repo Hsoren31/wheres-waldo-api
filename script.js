d12a12c67f37f56ffba0c3fc45e3577633c35489
Hsoren31 <sorensenhaley@gmail.com>
2026-03-10 12:40:03 -0600

    REFACTOR: update import for prisma

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-import "dotenv/config";
-import { PrismaClient } from "./generated/prisma/client.js";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const connectionString = process.env.DATABASE_URL;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+import { prisma } from "./lib/prisma.js";
 
 async function main() {
   await prisma.stage.deleteMany();
