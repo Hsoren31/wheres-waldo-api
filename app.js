e6e7415700e8f94005fd388adc1e2633f6f1d09f
Hsoren31 <sorensenhaley@gmail.com>
2026-03-11 11:02:57 -0600

    FEAT: add vercel url to cors origin

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173" || "https://where-s-waldo-gamma.vercel.app",
     credentials: true,
   })
 );
