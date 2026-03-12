49be9fde23db751a782ffa9b3d16db8060e48368
Hsoren31 <sorensenhaley@gmail.com>
2026-03-12 11:58:05 -0600

    FIX: update frontend url in cors array

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://where-s-waldo-gamma.vercel.app"],
+    origin: ["http://localhost:5173", "https://where-s-waldo-lime.vercel.app/"],
     credentials: true,
   })
 );
