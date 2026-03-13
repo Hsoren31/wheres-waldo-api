f714eae2262c0f376b50e176855356dc2f285c85
Hsoren31 <sorensenhaley@gmail.com>
2026-03-13 11:43:01 -0600

    FEAT: create environment variable for frontend url

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://where-s-waldo-lime.vercel.app/"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   })
 );
